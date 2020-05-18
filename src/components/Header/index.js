6fc898a5c32e0fcd939b4bcb8512360ead184dcb
wely88 <wely.vfr@gmail.com>
2020-05-18 23:20:43 +0300

    create React App

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { HeaderText } from './styles';
+
+function Header() {
+
+	return(
+		<HeaderText>MSI 2020</HeaderText>
+	);
+}
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { color } from '../../styles';
+
+export const HeaderText = styled.h1`
+   font-family: Roboto;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 20px;
+	line-height: 28px;
+    color: ${color.black};
+`;
