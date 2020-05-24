111bb8bae99b4fb3012fdb1343ec463fcd491f91
wely88 <wely.vfr@gmail.com>
2020-05-24 22:09:03 +0300

    Updates

---
+++ precache-manifest.fa162626054c470eae2922ef48b694f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89468105f53dcd8cc7b967dc8374f15",
+    "url": "/macpaw-test/index.html"
+  },
+  {
+    "revision": "c4ec7f9022e341ac2b7c",
+    "url": "/macpaw-test/static/js/2.666bf868.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/macpaw-test/static/js/2.666bf868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ffff3f17791a1c8fa75",
+    "url": "/macpaw-test/static/js/main.6411dabd.chunk.js"
+  },
+  {
+    "revision": "ab0bd145991a5bf48f76",
+    "url": "/macpaw-test/static/js/runtime-main.f8366a7b.js"
+  }
+]);
